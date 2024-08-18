0f825396452afd0a5a37af2b34224fdbf2d398d1
TheoRusu <theorusu11@gmail.com>
2024-08-18 19:30:19 -0400

    Project Completed

--- data-service-auth.js
+++ data-service-auth.js
@@ -3,97 +3,102 @@ var Schema = mongoose.Schema;
 const bcrypt = require('bcryptjs');
 
 var userSchema = new Schema({
-	userName: {
-		type: String,
-		unique: true
-	},
-	password: String,
-	email: String,
-	loginHistory: [ { dateTime: Date, userAgent: String } ]
+  userName: {
+    type: String,
+    unique: true,
+  },
+  password: String,
+  email: String,
+  loginHistory: [{ dateTime: Date, userAgent: String }],
 });
 
 let User;
 
-module.exports.initialize = function() {
-	return new Promise((resolve, reject) => {
-		let db = mongoose.createConnection(
-			'mongodb+srv://dbUser:WebSkills123@senecaweb.j6b02.mongodb.net/a6?retryWrites=true&w=majority'
-		);
-		db.on('error', (err) => {
-			reject(err);
-		});
-		db.once('open', () => {
-			User = db.model('users', userSchema);
-			resolve();
-		});
-	}); // promise end
+module.exports.initialize = function () {
+  return new Promise((resolve, reject) => {
+    let db = mongoose.createConnection(process.env.MONGO_URI);
+    db.on('error', (err) => {
+      reject(err);
+    });
+    db.once('open', () => {
+      User = db.model('users', userSchema);
+      resolve();
+    });
+  }); // promise end
 }; //initialize
 
-module.exports.registerUser = function(userData) {
-	return new Promise((resolve, reject) => {
-		if (!userData.password || !userData.password.trim() || !userData.password2 || !userData.password2.trim()) {
-			console.log(userData.password);
-			reject('Error: user name cannot be empty or only white spaces!');
-		} else if (userData.password != userData.password2) {
-			reject('Error: Passwords do not match');
-		} else {
-			let newUser = new User(userData);
-			bcrypt.genSalt(10, function(err, salt) {
-				bcrypt.hash(userData.password, salt, function(err, hashValue) {
-					if (err) {
-						reject('There was an error encrypting the password');
-					} else {
-						newUser.password = hashValue;
-						newUser.save((err) => {
-							if (err && err.code === 11000) {
-								reject('User Name already taken');
-							} else if (err) {
-								reject(`There was an error creating the user: ${err}`);
-							} else {
-								resolve();
-							}
-						});
-					}
-				});
-			});
-		}
-	});
+module.exports.registerUser = function (userData) {
+  return new Promise((resolve, reject) => {
+    if (
+      !userData.password ||
+      !userData.password.trim() ||
+      !userData.password2 ||
+      !userData.password2.trim()
+    ) {
+      console.log(userData.password);
+      reject('Error: user name cannot be empty or only white spaces!');
+    } else if (userData.password != userData.password2) {
+      reject('Error: Passwords do not match');
+    } else {
+      let newUser = new User(userData);
+      bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(userData.password, salt, function (err, hashValue) {
+          if (err) {
+            reject('There was an error encrypting the password');
+          } else {
+            newUser.password = hashValue;
+            newUser.save((err) => {
+              if (err && err.code === 11000) {
+                reject('User Name already taken');
+              } else if (err) {
+                reject(`There was an error creating the user: ${err}`);
+              } else {
+                resolve();
+              }
+            });
+          }
+        });
+      });
+    }
+  });
 };
 
-module.exports.checkUser = function(userData) {
-	return new Promise((resolve, reject) => {
-		User.find({ userName: userData.userName })
-			.exec()
-			.then((foundUser) => {
-				if (foundUser.length < 1) {
-					reject(`Unable to find user: ${userData.userName}`);
-				} else {
-					bcrypt.compare(userData.password, foundUser[0].password).then((res) => {
-						if (res === true) {
-							foundUser[0].loginHistory.push({
-								dateTime: new Date().toString(),
-								userAgent: userData.userAgent
-							});
-							User.updateOne(
-								{ userName: foundUser[0].userName },
-								{ $set: { loginHistory: foundUser[0].loginHistory } }
-							)
-								.exec()
-								.then(() => {
-									resolve(foundUser[0]);
-								})
-								.catch((err) => {
-									reject(`There
+module.exports.checkUser = function (userData) {
+  return new Promise((resolve, reject) => {
+    User.find({ userName: userData.userName })
+      .exec()
+      .then((foundUser) => {
+        if (foundUser.length < 1) {
+          reject(`Unable to find user: ${userData.userName}`);
+        } else {
+          bcrypt
+            .compare(userData.password, foundUser[0].password)
+            .then((res) => {
+              if (res === true) {
+                foundUser[0].loginHistory.push({
+                  dateTime: new Date().toString(),
+                  userAgent: userData.userAgent,
+                });
+                User.updateOne(
+                  { userName: foundUser[0].userName },
+                  { $set: { loginHistory: foundUser[0].loginHistory } }
+                )
+                  .exec()
+                  .then(() => {
+                    resolve(foundUser[0]);
+                  })
+                  .catch((err) => {
+                    reject(`There
                             was an error verifying the user: ${err}`);
-								});
-						} else {
-							reject(`Unable to find user: ${userData.userName}`);
-						}
-					});
-				}
-			})
-			.catch(() => {
-				reject(`Unable to find user: ${userData.userName}`);
-			});
-	});
+                  });
+              } else {
+                reject(`Unable to find user: ${userData.userName}`);
+              }
+            });
+        }
+      })
+      .catch(() => {
+        reject(`Unable to find user: ${userData.userName}`);
+      });
+  });
 };
--- data-service.js
+++ data-service.js
@@ -1,254 +1,258 @@
 const Sequelize = require('sequelize');
 
 var sequelize = new Sequelize(
-	'd2674hnmvt0scd',
-	'uurfkdtdgmdari',
-	'76a044728ae48c4dae06d3494fd41394e3a3e3bc64d9d88dd0ddb70d1cfb41ff',
-	{
-		host: 'ec2-52-54-38-229.compute-1.amazonaws.com',
-		dialect: 'postgres',
-		port: 5432,
-		dialectOptions: {
-			ssl: {
-				require: true,
-				rejectUnauthorized: false
-			}
-		}
-	}
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
+    dialectOptions: {
+      ssl: {
+        require: process.env.DB_SSL_REQUIRE === 'true',
+        rejectUnauthorized: process.env.DB_SSL_REJECT_UNAUTHORIZED === 'true',
+      },
+    },
+  }
 );
 
 sequelize
-	.authenticate()
-	.then(() => console.log('Connection success.'))
-	.catch((err) => console.log('Unable to connect to DB.', err));
+  .authenticate()
+  .then(() => console.log('Connection success.'))
+  .catch((err) => console.log('Unable to connect to DB.', err));
 
 var Employee = sequelize.define(
-	'Employee',
-	{
-		employeeNum: {
-			type: Sequelize.INTEGER,
-			primaryKey: true,
-			autoIncrement: true
-		},
-		firstName: Sequelize.STRING,
-		lastName: Sequelize.STRING,
-		email: Sequelize.STRING,
-		SSN: Sequelize.STRING,
-		addressStreet: Sequelize.STRING,
-		addressCity: Sequelize.STRING,
-		addressState: Sequelize.STRING,
-		addressPostal: Sequelize.STRING,
-		maritalStatus: Sequelize.STRING,
-		isManager: Sequelize.BOOLEAN,
-		employeeManagerNum: Sequelize.INTEGER,
-		status: Sequelize.STRING,
-		department: Sequelize.INTEGER,
-		hireDate: Sequelize.STRING
-	},
-	{
-		createdAt: false,
-		updatedAt: false
-	}
+  'Employee',
+  {
+    employeeNum: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    firstName: Sequelize.STRING,
+    lastName: Sequelize.STRING,
+    email: Sequelize.STRING,
+    SSN: Sequelize.STRING,
+    addressStreet: Sequelize.STRING,
+    addressCity: Sequelize.STRING,
+    addressState: Sequelize.STRING,
+    addressPostal: Sequelize.STRING,
+    maritalStatus: Sequelize.STRING,
+    isManager: Sequelize.BOOLEAN,
+    employeeManagerNum: Sequelize.INTEGER,
+    status: Sequelize.STRING,
+    department: Sequelize.INTEGER,
+    hireDate: Sequelize.STRING,
+  },
+  {
+    createdAt: false,
+    updatedAt: false,
+  }
 );
 
 var Department = sequelize.define(
-	'Department',
-	{
-		departmentId: {
-			type: Sequelize.INTEGER,
-			primaryKey: true,
-			autoIncrement: true
-		},
-		departmentName: Sequelize.STRING
-	},
-	{
-		createdAt: false,
-		updatedAt: false
-	}
+  'Department',
+  {
+    departmentId: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    departmentName: Sequelize.STRING,
+  },
+  {
+    createdAt: false,
+    updatedAt: false,
+  }
 );
 
-module.exports.initialize = function() {
-	return new Promise((resolve, reject) => {
-		sequelize
-			.sync()
-			.then(() => {
-				resolve();
-			})
-			.catch(() => {
-				reject('Unable to sync DB.');
-			});
-	}); // promise end
+module.exports.initialize = function () {
+  return new Promise((resolve, reject) => {
+    sequelize
+      .sync()
+      .then(() => {
+        resolve();
+      })
+      .catch(() => {
+        reject('Unable to sync DB.');
+      });
+  }); // promise end
 }; //initialize
 
-module.exports.getAllEmployees = function() {
-	return new Promise((resolve, reject) => {
-		Employee.findAll()
-			.then((data) => {
-				resolve(data);
-			})
-			.catch((err) => {
-				reject('No results returned.');
-			}); //catch
-	}); // promise
+module.exports.getAllEmployees = function () {
+  return new Promise((resolve, reject) => {
+    Employee.findAll()
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((err) => {
+        reject('No results returned.');
+      }); //catch
+  }); // promise
 }; // getAllEmployees
 
-module.exports.getEmployeesByStatus = function(status_query) {
-	return new Promise((resolve, reject) => {
-		Employee.findAll({
-			where: { status: status_query }
-		})
-			.then((data) => {
-				resolve(data);
-			})
-			.catch((err) => {
-				reject('No query results.');
-			}); //catch
-	}); //promise
+module.exports.getEmployeesByStatus = function (status_query) {
+  return new Promise((resolve, reject) => {
+    Employee.findAll({
+      where: { status: status_query },
+    })
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((err) => {
+        reject('No query results.');
+      }); //catch
+  }); //promise
 };
 
-module.exports.getEmployeesByDepartment = function(department_query) {
-	return new Promise((resolve, reject) => {
-		Employee.findAll({
-			where: { department: department_query }
-		})
-			.then((data) => {
-				resolve(data);
-			})
-			.catch((err) => {
-				reject('No query results.');
-			}); //catch
-	}); // promise
+module.exports.getEmployeesByDepartment = function (department_query) {
+  return new Promise((resolve, reject) => {
+    Employee.findAll({
+      where: { department: department_query },
+    })
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((err) => {
+        reject('No query results.');
+      }); //catch
+  }); // promise
 };
 
-module.exports.getEmployeesByManager = function(manager_query) {
-	return new Promise((resolve, reject) => {
-		Employee.findAll({
-			where: { employeeManagerNum: manager_query }
-		})
-			.then((data) => {
-				resolve(data);
-			})
-			.catch((err) => {
-				reject('No results returned!');
-			}); //catch
-	}); // promise
+module.exports.getEmployeesByManager = function (manager_query) {
+  return new Promise((resolve, reject) => {
+    Employee.findAll({
+      where: { employeeManagerNum: manager_query },
+    })
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((err) => {
+        reject('No results returned!');
+      }); //catch
+  }); // promise
 };
 
-module.exports.getEmployeeByNum = function(value) {
-	return new Promise((resolve, reject) => {
-		Employee.findAll({
-			where: { employeeNum: value }
-		})
-			.then((data) => {
-				resolve(data[0]);
-			})
-			.catch((err) => {
-				reject('No result.');
-			});
-	});
+module.exports.getEmployeeByNum = function (value) {
+  return new Promise((resolve, reject) => {
+    Employee.findAll({
+      where: { employeeNum: value },
+    })
+      .then((data) => {
+        resolve(data[0]);
+      })
+      .catch((err) => {
+        reject('No result.');
+      });
+  });
 };
 
-module.exports.addEmployee = function(employeeData) {
-	return new Promise((resolve, reject) => {
-		// object employeeData is from user's input (req.query.body)
-		employeeData.isManager = employeeData.isManager ? true : false;
-		for (let prop in employeeData) {
-			if (employeeData[prop] == '') employeeData[prop] = null;
-		} // set '' to null for object property values
-		// object employeeData is ready to be created (added) to table (model) Employee
-		Employee.create(employeeData)
-			.then(() => {
-				resolve();
-			})
-			.catch((err) => {
-				reject('Cannot add employee.');
-			});
-	});
+module.exports.addEmployee = function (employeeData) {
+  return new Promise((resolve, reject) => {
+    // object employeeData is from user's input (req.query.body)
+    employeeData.isManager = employeeData.isManager ? true : false;
+    for (let prop in employeeData) {
+      if (employeeData[prop] == '') employeeData[prop] = null;
+    } // set '' to null for object property values
+    // object employeeData is ready to be created (added) to table (model) Employee
+    Employee.create(employeeData)
+      .then(() => {
+        resolve();
+      })
+      .catch((err) => {
+        reject('Cannot add employee.');
+      });
+  });
 };
 
-module.exports.updateEmployee = function(employeeData) {
-	return new Promise((resolve, reject) => {
-		employeeData.isManager = employeeData.isManager ? true : false;
-		for (let prop in employeeData) {
-			if (employeeData[prop] == '') employeeData[prop] = null;
-		}
-		Employee.update(employeeData, { where: { employeeNum: employeeData.employeeNum } })
-			.then(() => {
-				resolve();
-			})
-			.catch((err) => {
-				reject('cannot update.');
-			});
-	});
+module.exports.updateEmployee = function (employeeData) {
+  return new Promise((resolve, reject) => {
+    employeeData.isManager = employeeData.isManager ? true : false;
+    for (let prop in employeeData) {
+      if (employeeData[prop] == '') employeeData[prop] = null;
+    }
+    Employee.update(employeeData, {
+      where: { employeeNum: employeeData.employeeNum },
+    })
+      .then(() => {
+        resolve();
+      })
+      .catch((err) => {
+        reject('cannot update.');
+      });
+  });
 };
 
-module.exports.deleteEmployeeByNum = function(empNum) {
-	return new Promise((resolve, reject) => {
-		Employee.destroy({
-			where: { employeeNum: empNum }
-		})
-			.then(() => {
-				resolve();
-			})
-			.catch((err) => {
-				reject('Unable to delete employee.');
-			});
-	});
+module.exports.deleteEmployeeByNum = function (empNum) {
+  return new Promise((resolve, reject) => {
+    Employee.destroy({
+      where: { employeeNum: empNum },
+    })
+      .then(() => {
+        resolve();
+      })
+      .catch((err) => {
+        reject('Unable to delete employee.');
+      });
+  });
 };
 
 //department
-module.exports.getDepartments = function() {
-	return new Promise((resolve, reject) => {
-		Department.findAll()
-			.then((data) => {
-				resolve(data);
-			})
-			.catch((err) => {
-				reject('No results.');
-			}); //catch
-	}); // promise
+module.exports.getDepartments = function () {
+  return new Promise((resolve, reject) => {
+    Department.findAll()
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((err) => {
+        reject('No results.');
+      }); //catch
+  }); // promise
 };
 
-module.exports.addDepartment = function(departmentData) {
-	return new Promise((resolve, reject) => {
-		for (let prop in departmentData) {
-			if (departmentData[prop] == '') departmentData[prop] = null;
-		}
-		Department.create(departmentData)
-			.then(() => {
-				resolve();
-			})
-			.catch((err) => {
-				reject('Cannot add department');
-			}); // catch
-	}); // promise
+module.exports.addDepartment = function (departmentData) {
+  return new Promise((resolve, reject) => {
+    for (let prop in departmentData) {
+      if (departmentData[prop] == '') departmentData[prop] = null;
+    }
+    Department.create(departmentData)
+      .then(() => {
+        resolve();
+      })
+      .catch((err) => {
+        reject('Cannot add department');
+      }); // catch
+  }); // promise
 };
 
-module.exports.updateDepartment = function(departmentData) {
-	return new Promise((resolve, reject) => {
-		for (let prop in departmentData) {
-			if (departmentData[prop] == '') {
-				departmentData[prop] = null;
-			}
-		}
-		Department.update(departmentData, { where: { departmentId: departmentData.departmentId } })
-			.then(() => {
-				resolve();
-			})
-			.catch((err) => {
-				reject('Cannot update Department.');
-			}); // catch
-	}); // promise
+module.exports.updateDepartment = function (departmentData) {
+  return new Promise((resolve, reject) => {
+    for (let prop in departmentData) {
+      if (departmentData[prop] == '') {
+        departmentData[prop] = null;
+      }
+    }
+    Department.update(departmentData, {
+      where: { departmentId: departmentData.departmentId },
+    })
+      .then(() => {
+        resolve();
+      })
+      .catch((err) => {
+        reject('Cannot update Department.');
+      }); // catch
+  }); // promise
 };
 
-module.exports.getDepartmentById = function(id) {
-	return new Promise((resolve, reject) => {
-		Department.findAll({ where: { departmentId: id } })
-			.then((data) => {
-				resolve(data[0]);
-			})
-			.catch((err) => {
-				reject('Cannot find the department by Id.');
-			}); // catch
-	}); // promise
+module.exports.getDepartmentById = function (id) {
+  return new Promise((resolve, reject) => {
+    Department.findAll({ where: { departmentId: id } })
+      .then((data) => {
+        resolve(data[0]);
+      })
+      .catch((err) => {
+        reject('Cannot find the department by Id.');
+      }); // catch
+  }); // promise
 };
